bff489bece5412b2613189d212d8b9530dc24a77
Dan Davis <daniel.davis@nih.gov>
2021-06-11 14:06:50 -0400

    Begin updating to show how to use Z39.50 with Alma.

--- nlmquery.py
+++ nlmquery.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2
 import argparse
 import sys
 from os.path import basename
@@ -6,7 +7,8 @@ from PyZ3950 import zoom
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(prog = basename(__file__))
-    parser.add_argument('nlmui', nargs='+', type=int, help='Give an NLM UI to query')
+    parser.add_argument('identifier', nargs='+', type=int, help='Give an NLM UI to query')
+    parser.add_argument('--idtype', metavar='IDTYPE', choices=['nlmui', 'mmsid', 'issn', 'isbn'], default='nlmui')
     parser.add_argument('--host', metavar='HOSTNAME', default='ilsz3950.nlm.nih.gov')
     parser.add_argument('--port', metavar='PORT', default='7091')
     parser.add_argument('--database', metavar='NAME', default='VOYAGER')
@@ -23,8 +25,13 @@ def open_connection(opts):
     return conn
 
 
-def search(conn, nlmui):
-    query = zoom.Query('PQF', '@attr 1=1062 %s' % str(nlmui))
+def search(conn, idtype, identifier):
+    query_string = None
+    if idtype == 'nlmui':
+	query_string = '@attr 1=1062 %s' % str(identifier)
+    elif idtype == 'issn':
+        query_string = '@attr ' % str(identifier)
+    query = zoom.Query('PQF', query_string)
     res = conn.search(query)
     for r in res:
         print(str(r))
@@ -35,8 +42,8 @@ def main(argv):
     opts = parse_args(argv)
     conn = open_connection(opts)
 
-    for nlmui in opts.nlmui:
-        search(conn, nlmui)
+    for identifier in opts.identifier:
+        search(conn, opts.idtype, identifier)
 
 
 if __name__ == '__main__':
