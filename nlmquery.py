42e6d0b24c94a9ca5b1f26f48bd54d8d50868439
Dan Davis <daniel.davis@nih.gov>
2021-06-11 15:06:38 -0400

    Zoom client doesn't appear to work with Z39.50

--- nlmquery.py
+++ nlmquery.py
@@ -7,8 +7,10 @@ from PyZ3950 import zoom
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(prog = basename(__file__))
-    parser.add_argument('identifier', nargs='+', type=int, help='Give an NLM UI to query')
-    parser.add_argument('--idtype', metavar='IDTYPE', choices=['nlmui', 'mmsid', 'issn', 'isbn'], default='nlmui')
+    parser.add_argument('identifier', nargs='+', help='Give an NLM UI to query')
+    parser.add_argument('--type', metavar='IDTYPE',
+                        choices=['query', 'nlmui', 'mmsid', 'issn', 'isbn'],
+                        default='nlmui')
     parser.add_argument('--host', metavar='HOSTNAME', default='ilsz3950.nlm.nih.gov')
     parser.add_argument('--port', metavar='PORT', default='7091')
     parser.add_argument('--database', metavar='NAME', default='VOYAGER')
@@ -25,16 +27,18 @@ def open_connection(opts):
     return conn
 
 
-def search(conn, idtype, identifier):
+def search(conn, query_type, identifier):
     query_string = None
-    if idtype == 'nlmui':
-        query_string = '@attr 1=1062 %s' % str(identifier)
-    elif idtype == 'issn':
-        query_string = '@attr 1=8 %s' % str(identifier)
-    elif idtype == 'isbn':
-        query_string = '@attr 1=7 %s' % str(identifier)
+    if query_type == 'nlmui':
+        query_string = '@attr 1=1062 @attr 2=3 "%s"' % str(identifier)
+    elif query_type == 'issn':
+        query_string = '@attr 1=8 @attr 2=3 "%s"' % str(identifier)
+    elif query_type == 'isbn':
+        query_string = '@attr 1=7 @attr 2=3 "%s"' % str(identifier)
+    elif query_type == 'query':
+        query_string = identifier
     else:
-        query_string = '@attr 1=12 2=3 %s' % str(identifier)
+        query_string = '@attr 1=12 @attr 2=3 "%s"' % str(identifier)
     query = zoom.Query('PQF', query_string)
     res = conn.search(query)
     for r in res:
@@ -47,7 +51,7 @@ def main(argv):
     conn = open_connection(opts)
 
     for identifier in opts.identifier:
-        search(conn, opts.idtype, identifier)
+        search(conn, opts.type, identifier)
 
 
 if __name__ == '__main__':
